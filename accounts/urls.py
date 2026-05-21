1206d4b77620aaf56418fbcb0a5c7c7cd44a7c09
bubaa001 <egliszaratus@gmail.com>
2026-05-21 15:31:36 +0300

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_view, name="home"),
+    path('', views.home_view, name="home"),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('select-level/', views.select_level_view, name='select_level'),
